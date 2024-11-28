cfba158b4fd408bb6cf4fc19fdd2d2715b98a950
root <root@VMPruebas1>
2024-11-28 19:36:06 +0000

    Punto B resuelto, modificaciones en el codigo

--- RTA_Examen_20241116/Punto_B.sh
+++ RTA_Examen_20241116/Punto_B.sh
@@ -1,57 +1,19 @@
 #!/bin/bash
 
-ruta_destino="/usr/local/bin/BevilacquaAltaUser-Groups.sh"
 
-if [ "$(realpath "$0")" != "$ruta_destino" ]; then
-    echo "El script no está en la ubicación correcta. Moviéndolo a $ruta_destino..."
-    
-    sudo cp "$0" "$ruta_destino"
-    
-    sudo chmod +x "$ruta_destino"
-    
-    echo "El script se ha movido y se ha dado permisos de ejecución."
-    echo "Ejecute el script nuevamente desde $ruta_destino."
-    
-    exit 0
-fi
+groupadd grupo1
+groupadd grupo2
+groupadd grupo3
 
-if [ $# -ne 2 ]; then
-    echo "Uso: $0 <usuario_existente> <ruta_Lista_Usuarios.txt>"
-    exit 1
-fi
 
-usuario_existente=$1
-archivo_lista=$2
+useradd -m -p $(openssl passwd -6 'contraseña_segura1') usuario1
+usermod -aG grupo1 usuario1
+echo "usuario1:grupo1" >> Lista_Usuarios.txt
 
-if [ ! -f "$archivo_lista" ]; then
-    echo "Error: El archivo $archivo_lista no existe."
-    exit 1
-fi
+useradd -m -p $(openssl passwd -6 'contraseña_segura2') usuario2
+usermod -aG grupo2 usuario2
+echo "usuario2:grupo2" >> Lista_Usuarios.txt
 
-clave_encriptada=$(sudo getent shadow "$usuario_existente" | cut -d: -f2)
-
-if [ -z "$clave_encriptada" ]; then
-    echo "Error: No se pudo obtener la contraseña del usuario $usuario_existente"
-    exit 1
-fi
-
-while read -r linea; do
-    usuario=$(echo "$linea" | cut -d: -f1)
-    grupo=$(echo "$linea" | cut -d: -f2)
-
-    if ! getent group "$grupo" > /dev/null; then
-        sudo groupadd "$grupo"
-        echo "Grupo $grupo creado."
-    fi
-
-    if ! id "$usuario" > /dev/null 2>&1; then
-        sudo useradd -m -g "$grupo" "$usuario"
-        echo "$usuario:$clave_encriptada" | sudo chpasswd -e
-        echo "Usuario $usuario creado y asignado al grupo $grupo."
-    else
-        echo "El usuario $usuario ya existe, omitiendo."
-    fi
-done < "$archivo_lista"
-
-echo "Procesamiento completo."
-
+useradd -m -p $(openssl passwd -6 'contraseña_segura3') usuario3
+usermod -aG grupo3 usuario3
+echo "usuario3:grupo3" >> Lista_Usuarios.txt
